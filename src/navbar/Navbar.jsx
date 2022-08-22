39bc359fd6fe7d73b187ea431bb3c7e580db71fb
Carin Wood <carin.wood.85@gmail.com>
2022-08-22 19:09:23 +0200

    links update

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import styles from './navbar.module.css'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
-        <p>Search Country</p>
-        <h1 className={styles.countries}>List of Countries</h1>
+        <Link to="/search">Search Country</Link>
+        <Link to="/"><h1 className={styles.countries}>List of Countries</h1></Link>
     </nav>
   )
 }
